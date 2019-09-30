821b3a74cc5da70f9519fc06c74c9588e8acb747
flatironschool <flatironschool@flatironschools-MacBook-Air.local>
2019-09-29 20:46:02 -0400

    001, 002 migrated

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -8,13 +8,13 @@
 # In this change method, create columns with the correct names and 
 # value types according to the spec
 
-class CreateCostumes < ActiveRecord::Migration[5.2.3]
+class CreateCostumes < ActiveRecord::Migration[5.2]
     def change
-        create_column :costumes do |t|
+        create_table :costumes do |t|
             t.string :name
             t.integer :price
             t.string :image_url
-            t.string :medium
+            t.string :size
             t.timestamps
         end
     end
